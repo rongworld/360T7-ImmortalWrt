721abac26aaf4984265cc52b0f0cb7cea2f4020d
pc <2238114323@qq.com>
2026-08-27 08:44:48 +0500

    feat: slim 360T7 firmware config

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Auto build OpenWrt firmware for 360T7 via GitHub Actions
 
 | Default ip | Default user | Default password |
 | --- | --- | --- | 
-| 192.168.233.1 | root | - |
+| 192.168.2.1 | root | - |
 
 Change Repo to [immortalwrt-mt798x-24.10](https://github.com/padavanonly/immortalwrt-mt798x-6.6)
 BRANCH: openwrt-24.10-6.6
--- diy-part1.sh
+++ diy-part1.sh
@@ -8,15 +8,4 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
-
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
-
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-
-#sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
-
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+# No additional feeds are required for this image.
