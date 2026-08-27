0f9f003088fd48c0379cdb19e5ef2708c4417b37
pc <2238114323@qq.com>
2026-08-27 09:16:09 +0500

    fix: harden 360T7 upgrade patch

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-#============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#============================================================
+set -e
 
 # Modify default IP
-sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.2.1/g' \
+    package/base-files/files/bin/config_generate
 
 # Keep the legacy 21.02 board name accepted by sysupgrade metadata checks.
 DEVICE_DEFINITION='target/linux/mediatek/image/filogic.mk'
-if ! grep -qE '^[[:space:]]*SUPPORTED_DEVICES[[:space:]]*\+= 360,t7[[:space:]]*$' "$DEVICE_DEFINITION"; then
-    sed -i '/^define Device\/qihoo_360t7$/a\  SUPPORTED_DEVICES += 360,t7' "$DEVICE_DEFINITION"
+
+if ! grep -q '^define Device/qihoo_360t7$' "$DEVICE_DEFINITION"; then
+    echo "ERROR: qihoo_360t7 device definition not found"
+    exit 1
 fi
 
+if ! sed -n '/^define Device\/qihoo_360t7$/,/^endef$/p' "$DEVICE_DEFINITION" | grep -qE '^[[:space:]]*SUPPORTED_DEVICES[[:space:]]*\+= 360,t7[[:space:]]*$'; then
+
+    sed -i '/^define Device\/qihoo_360t7$/a\  SUPPORTED_DEVICES += 360,t7' \
+        "$DEVICE_DEFINITION"
+fi
+
+echo "360T7 device definition after patch:"
+sed -n '/^define Device\/qihoo_360t7$/,/^endef$/p' "$DEVICE_DEFINITION"
+
 # Modify hostname
-#sed -i 's/OpenWrt/360T7/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/360T7/g' \
+#     package/base-files/files/bin/config_generate
