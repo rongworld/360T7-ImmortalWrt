73e4a7e34cdbc6c0f47f85d9348e1e9d6bb95a70
pc <2238114323@qq.com>
2026-08-27 09:09:20 +0500

    fix: keep legacy 360T7 sysupgrade alias

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,11 @@
 # Modify default IP
 sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
+# Keep the legacy 21.02 board name accepted by sysupgrade metadata checks.
+DEVICE_DEFINITION='target/linux/mediatek/image/filogic.mk'
+if ! grep -qE '^[[:space:]]*SUPPORTED_DEVICES[[:space:]]*\+= 360,t7[[:space:]]*$' "$DEVICE_DEFINITION"; then
+    sed -i '/^define Device\/qihoo_360t7$/a\  SUPPORTED_DEVICES += 360,t7' "$DEVICE_DEFINITION"
+fi
+
 # Modify hostname
 #sed -i 's/OpenWrt/360T7/g' package/base-files/files/bin/config_generate
